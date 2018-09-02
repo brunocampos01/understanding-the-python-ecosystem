2f62dfcd2efb03a5a2113f7723975fd0cbec3d99
Bruno Campos <brunocampos01@gmail.com>
2018-09-02 12:49:49 -0300

    Update aula006-primitive types.py

--- aulas/aula006-primitive types.py
+++ aulas/aula006-primitive types.py
@@ -19,9 +19,8 @@ if False:
     n1 = int(input('enter value: '))
     n2 = int(input('enter other value: '))
     s = n1 + n2
-    print('the sum is', n1, 'and ', n2, 'worth', s)  # method 1, exceeded, Python 2
-    print('the sum is {} and {} worth {}'.format(n1, n2, s))  # method 2
-    print('the sum is {0} and {1} worth {2}'.format(n1, n2, s))  # method 3
+    print('the sum between {} and {} is {}'.format(n1, n2, s))  # method 1
+    print(f'the sum between {n1} and {n2} is {s}')  # method f strings
 
 #checkers types
 if False:
