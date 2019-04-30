b56625b2157ddb084c52e4594a38b9183c5f0197
Bruno Campos <brunocampos01@gmail.com>
2019-04-30 08:08:42 -0300

    Update mysql_helper.py

--- databases/mysql_helper.py
+++ databases/mysql_helper.py
@@ -10,13 +10,13 @@ PATH_CONFIG = "/conf/prod/luigi.cfg"
 config = configparser.ConfigParser()
 config.read(PATH_PROJECT + PATH_CONFIG)
 
-MYSQL_USER_COCKPIT = config['RDSMySQL']['MYSQL_USER_COCKPIT']
-MYSQL_PASSWORD_COCKPIT = config['RDSMySQL']['MYSQL_PASSWORD_COCKPIT']
-MYSQL_USER = config['RDSMySQL']['MYSQL_USER']
-MYSQL_PASSWORD = config['RDSMySQL']['MYSQL_PASSWORD']
-HOST_MYSQL = config['RDSMySQL']['HOST_MYSQL']
-MYSQL_DB_NAME = config['RDSMySQL']['MYSQL_DB_NAME']
-PATH_SCRIPT_SQL = config['RDSMySQL']['PATH_SCRIPT_SQL']
+MYSQL_USER_COCKPIT = config['RDSMySQL']['']
+MYSQL_PASSWORD_COCKPIT = config['RDSMySQL']['']
+MYSQL_USER = config['RDSMySQL']['']
+MYSQL_PASSWORD = config['RDSMySQL']['']
+HOST_MYSQL = config['RDSMySQL']['']
+MYSQL_DB_NAME = config['RDSMySQL']['']
+PATH_SCRIPT_SQL = config['RDSMySQL']['']
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s',
