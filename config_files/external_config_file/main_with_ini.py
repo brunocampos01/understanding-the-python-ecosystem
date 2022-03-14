1590ad91d225a033483a828919a269cfbb767436
Bruno Campos <brunocampos01@gmail.com>
2022-03-14 03:13:51 -0300

    Update main_with_ini.py

--- config_files/external_config_file/main_with_ini.py
+++ config_files/external_config_file/main_with_ini.py
@@ -5,3 +5,7 @@ config.read('config.ini')
 
 secret_key = config['DEFAULT']['SECRET_KEY']    # 'secret-key-of-myapp'
 ci_hook_url = config['CI']['HOOK_URL']  # 'web-hooking-url-from-ci-service'
+
+# other form
+secret_key = config.get("DEFAULT", "SECRET_KEY")
+ci_hook_url = config.get('CI', 'HOOK_URL')
